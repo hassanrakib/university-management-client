43ce71aed9d2e3fa70b0ae176355e3ed41b8c690
Rakib Hassan <mailrakibhassan@gmail.com>
2024-11-10 08:13:54 +0600

    PHForm.tsx & PHInput.tsx created

--- src/components/form/PHForm.tsx
+++ src/components/form/PHForm.tsx
@@ -1,8 +1,31 @@
-import { useForm } from "react-hook-form";
+import React from "react";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
 
-const PHForm = ({ onSubmit, children }) => {
-  const { handleSubmit } = useForm();
-  return <form onSubmit={handleSubmit(onSubmit)}>{children}</form>;
+type FormConfig = {
+  defaultValues?: Record<string, unknown>
+}
+
+type PHFormProps = {
+  onSubmit: SubmitHandler<FieldValues>;
+  children: React.ReactNode;
+} & FormConfig;
+
+const PHForm = ({ onSubmit, children, defaultValues }: PHFormProps) => {
+  const formConfig: FormConfig = {};
+  if(defaultValues) {
+    formConfig["defaultValues"] = defaultValues;
+  }
+  const methods = useForm(formConfig);
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  );
 };
 
 export default PHForm;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,4 @@
-import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { Button, Row } from "antd";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { useAppDispatch } from "../redux/hooks";
 import { setUser, User } from "../redux/features/auth/authSlice";
@@ -7,74 +6,69 @@ import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import PHForm from "../components/form/PHForm";
+import PHInput from "../components/form/PHInput";
+import { FieldValues } from "react-hook-form";
 
 export default function Login() {
+  // define login form default values
+  const defaultValues = {
+    userId: "A-0001",
+    password: "admin123",
+  };
+
   const dispatch = useAppDispatch();
 
   // navigate function is taken from react-router-dom to programmatically navigate
   const navigate = useNavigate();
 
-  // get the register function from react-hook-form
-  const { register } = useForm({
-    defaultValues: {
-      userId: "A-0001",
-      password: "admin123",
-    },
-  });
-
   // the first variable is the function to do the mutation
   const [login] = useLoginMutation();
 
   // data : {userId: string, password: string} because of using react-hook-form
-  const onSubmit = async (data: { userId: string; password: string }) => {
-    console.log(data);
-    // // show the loading toast
-    // const loadingToastId = toast.loading("Logging in...");
+  const onSubmit = async (data: FieldValues) => {
+    // show the loading toast
+    const loadingToastId = toast.loading("Logging in...");
 
-    // try {
-    //   const loginCredentials = {
-    //     id: data.userId,
-    //     password: data.password,
-    //   };
+    try {
+      const loginCredentials = {
+        id: data.userId,
+        password: data.password,
+      };
 
-    //   // instead of getting the result like this {data: {...data}} get this {...data} actual data using unwrap()
-    //   const result: { data: { accessToken: string } } = await login(
-    //     loginCredentials
-    //   ).unwrap();
+      // instead of getting the result like this {data: {...data}} get this {...data} actual data using unwrap()
+      const result: { data: { accessToken: string } } = await login(
+        loginCredentials
+      ).unwrap();
 
-    //   // decode token to get the user: {userId: "", role: ""}
-    //   const user = verifyToken(result.data.accessToken) as User;
+      // decode token to get the user: {userId: "", role: ""}
+      const user = verifyToken(result.data.accessToken) as User;
 
-    //   // set the user to the local state
-    //   dispatch(setUser({ user, token: result.data.accessToken }));
+      // set the user to the local state
+      dispatch(setUser({ user, token: result.data.accessToken }));
 
-    //   // redirect after login
-    //   navigate(`/${user.role}/dashboard`);
+      // redirect after login
+      navigate(`/${user.role}/dashboard`);
 
-    //   // replace the loading toast using the second argument
-    //   toast.success("Logged in successfully!", {
-    //     id: loadingToastId,
-    //     duration: 2000,
-    //   });
-    // } catch (err) {
-    //   toast.error("Something went wrong!", {
-    //     id: loadingToastId,
-    //     duration: 2000,
-    //   });
-    // }
+      // replace the loading toast using the second argument
+      toast.success("Logged in successfully!", {
+        id: loadingToastId,
+        duration: 2000,
+      });
+    } catch (err) {
+      toast.error("Something went wrong!", {
+        id: loadingToastId,
+        duration: 2000,
+      });
+    }
   };
 
   return (
-    <PHForm onSubmit={onSubmit}>
-      <div>
-        <label htmlFor="id">ID:</label>
-        <input type="text" id="id" {...register("userId")} />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="text" id="password" {...register("password")} />
-      </div>
-      <Button htmlType="submit">Login</Button>
-    </PHForm>
+    <Row justify="center" align="middle" style={{ minHeight: "100vh" }}>
+      <PHForm onSubmit={onSubmit} defaultValues={defaultValues}>
+        <PHInput type="text" name="userId" label="ID:" />
+        <PHInput type="password" name="password" label="Password:" />
+        <Button htmlType="submit">Login</Button>
+      </PHForm>
+    </Row>
   );
 }
